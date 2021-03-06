62fe6a6967a9edf63838befc219596811bbacfeb
Parsa Hosseini <parsa@firstsource.tech>
2021-03-06 17:09:37 +0330

    Added cors module to the services and connected the client to the services

---
+++ client/lib/widgets/post_create.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PostCreate extends StatefulWidget {
+  @override
+  _PostCreateState createState() => _PostCreateState();
+}
+
+class _PostCreateState extends State<PostCreate> {
+  final _controller = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Create Post",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(height: 15),
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(horizontal: 12),
+              labelText: "Title",
+            ),
+          ),
+          SizedBox(height: 15),
+          RaisedButton(
+            child: Text("Submit"),
+            onPressed: _isLoading ? null : submit,
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> submit() async {
+    print(_controller.text.runtimeType);
+    if (_controller.text == "") return;
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      http.Response response = await http.post(
+        "http://10.0.2.2:4000/posts",
+        headers: {"Content-Type": "application/json; charset=UTF-8"},
+        body: jsonEncode({"title": _controller.text}),
+      );
+    } catch (e) {
+      print(e);
+    }
+
+    _controller.clear();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
